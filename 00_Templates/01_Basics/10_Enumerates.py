b99f0bb2f153851845d5ed73583e0e77affd06f1
James <daltdoerfer@yahoo.com>
2023-11-15 11:04:53 +0100

    Kommentare ergänzt

--- 00_Templates/01_Basics/10_Enumerates.py
+++ 00_Templates/01_Basics/10_Enumerates.py
@@ -1,5 +1,8 @@
 # Enumerates, einfaches Indexieren und Iterieren von Daten
 # https://book.pythontips.com/en/latest/enumerate.html
+# Python's enumerate method allows you to iterate over a list, tuple,
+# or dictionary and return a tuple containing the index of each element and the element itself.
+# This is a convenient way to keep track of both the element and its position on a list.
 
 smaller = [1,2,3]
 names = ['jan','peter','horst']
@@ -8,12 +11,15 @@ names = ['jan','peter','horst']
 for idx, name in enumerate(names, 0): # Beginnt bei 0
     print(idx, name)
 
+print("\n")
+
 # Macht das gleiche wie oben, nur wird zusätzlich die ZIP Funktion vorher ausgeführt.
 # -> Es wird eine Liste aus Smaller und Names erstellt dann zusätzlich iteriert
 # Ausgabe: Nummer, Index, Name
 for number, (index_names,name) in zip(smaller, enumerate(names, 0)):
     print(number,index_names, name)
 
+print("\n")
 
 # Beispiel mit enumerate und zip anders herum
 # Ausgabe: Index Zuerst, dann Nummer, dann Name
--- 00_Templates/03_Module/PYNPUT_Tastaturbefehle/AFK_BOT/main.py
+++ 00_Templates/03_Module/PYNPUT_Tastaturbefehle/AFK_BOT/main.py
@@ -12,9 +12,9 @@ try:
 	print('AFK script started, press "%s" key to start/stop bot.' % toggle_button)
 
 	while True:		
-		key_down = keyboard.is_pressed(toggle_button)
-		if key_down != last_state:
-			last_state = key_down
+		key_down = keyboard.is_pressed(toggle_button) # Prüft ob Toggle Button gedrückt -> Returns True wenn gedrückt
+		if key_down != last_state: # Wenn erstmalig gedrückt, dann !=
+			last_state = key_down  # Übernimmt aktuellen Zustand. Wenn erstmalig toggle button gedrücked dann TRUE
 			if last_state:
 				enabled = not enabled
 				if enabled and first_time:
